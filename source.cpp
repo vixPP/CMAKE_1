d1ccdb589b56ac8221d322a212f3744986fdae3a
vix <churink9@gmail.com>
2024-04-25 19:09:48 +0300

    first commit

---
+++ source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	string name;
+
+	cout << "Введите имя: ";
+	cin >> name;
+	cout << "Здравствуйте, " << name << '!';
+	
+	return 0;
+}
